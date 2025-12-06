0feda8f97ab7874f58a8d039185f7c0859af8b24
mrhighdot <gabrielomane@gmail.com>
2025-12-06 09:23:43 +0000

    Initialized project

---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import { Context, Hono } from "hono";
+import payload from "./files/payload.json";
+
+const app = new Hono();
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/get-blogs", async (c: Context) => {
+  return c.json(payload);
+});
+
+app.get("/blogs/:slug", async (c: Context) => {
+  const slug = c.req.param("slug");
+
+  // const actualPayload = JSON.parse(payload);
+  const getSelectedBlog = payload.posts?.find(
+    (item: any) => item.slug === slug,
+  );
+
+  if (!getSelectedBlog) {
+    return c.json({ message: "Blog not found" }, 404);
+  }
+
+  return c.json(getSelectedBlog);
+});
+
+export default app;
